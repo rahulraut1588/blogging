e066afddd909e1b131b3683c7e2eab98e447ca02
Rahul Raut <rahulraut.1588@gmail.com>
2019-09-01 19:11:30 +0530

    Implemented front end design for authentication form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'public';
+  formFlag: boolean = false;
+
+  changeFlag() {
+    this.formFlag = (this.formFlag) ? false : true;
+    console.log(this.formFlag);
+  }
+
+  loginSubmit(loginData) {
+    console.log(loginData);
+  }
+
+  regSubmit(regData) {
+    console.log(regData);
+  }
 }
